697dcc8fa7a7027e1d61b6a3d4bd506a3bfe546e
Jan Dosoudil <dosoudilj@u-2.cz>
2011-09-27 15:42:04 +0200

    Vypisování adres paměti

--- 01/pb173.c
+++ 01/pb173.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/device.h>
 
 static int my_init(void)
 {
@@ -13,6 +14,14 @@ static void my_exit(void)
 	if (mem) {
 		strcpy(mem, "bye");
 		printk(KERN_INFO "%s", mem);
+		
+		printk(KERN_INFO "kmalloc: %pS", mem);
+		char a = 'A';
+		printk(KERN_INFO "stack: %p", &a);
+		printk(KERN_INFO "jiffies: %p", &jiffies);
+		printk(KERN_INFO "function: %p", my_init);
+		printk(KERN_INFO "function2: %p", bus_register);
+		printk(KERN_INFO "function2: %pF", __builtin_return_address(0));
 	} else {
 		printk(KERN_INFO "nelze alokovat");
 	}
