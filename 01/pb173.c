03a3e0f40bf467a79d90e15ae4b3c9d0b1fc966c
Jan Dosoudil <dosoudilj@u-2.cz>
2011-09-27 15:02:41 +0200

    Jednoduchý modul + paměť

--- 01/pb173.c
+++ 01/pb173.c
@@ -1,12 +1,22 @@
 #include <linux/module.h>
+#include <linux/slab.h>
 
 static int my_init(void)
 {
+	printk(KERN_INFO "Hello world");
 	return 0;
 }
 
 static void my_exit(void)
 {
+	void* mem = kmalloc(1000, GFP_KERNEL);
+	if (mem) {
+		strcpy(mem, "bye");
+		printk(KERN_INFO "%s", mem);
+	} else {
+		printk(KERN_INFO "nelze alokovat");
+	}
+	kfree(mem);
 }
 
 module_init(my_init);
